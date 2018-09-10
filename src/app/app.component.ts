e823ed333daf6b64dc7f8d9dc40b21f98da4a1bb
Ryan Efendy <ryefendy@microsoft.com>
2018-09-10 08:40:19 -0700

    Finish section 5 :sparkles::sparkles::sparkles:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public showRecipe = true;
+
+  public onNotifyClicked(message: string): void {
+    this.showRecipe = message === 'recipe' ? true : false;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() notify: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleHeader(header: string): void {
+    this.notify.emit(header);
   }
 
 }
