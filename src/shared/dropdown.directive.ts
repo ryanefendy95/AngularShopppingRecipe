f4092c15e4092c90fba18c5748d4855040f7224a
Ryan Efendy <ryanefendy95@gmail.com>
2018-10-24 23:39:57 -0700

    Complete module 8 :tada::tada::tada:

--- src/shared/dropdown.directive.ts
+++ src/shared/dropdown.directive.ts
@@ -1,8 +1,14 @@
-import { Directive } from "@angular/core";
+import { Directive, HostListener, Renderer2, ElementRef, HostBinding } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirectives {
+    // constructor(private elRef: ElementRef, private renderer: Renderer2){ }
+    @HostBinding('class.open') isOpen = false;
 
+    @HostListener('click') toggleOpen(eventData: Event) {
+        // this.renderer.setProperty(this.elRef.nativeElement, 'open', true);
+        this.isOpen = !this.isOpen;
+    };
 }
